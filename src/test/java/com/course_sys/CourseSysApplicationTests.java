562a550adf7693d8602280b31968f18dc32ea628
UlrikhSergey <ulrikh.sergey@gmail.com>
2023-04-14 13:45:49 +0300

    commit 1

--- src/test/java/com/course_sys/CourseSysApplicationTests.java
+++ src/test/java/com/course_sys/CourseSysApplicationTests.java
@@ -2,6 +2,7 @@ package com.course_sys;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 class CourseSysApplicationTests {
