fe44bc6c484313160f7a33da71a36de1526d0b5e
UlrikhSergey <ulrikh.sergey@gmail.com>
2023-04-18 11:44:40 +0300

    commit 1

--- src/main/java/com/course_sys/controller/FilesController.java
+++ src/main/java/com/course_sys/controller/FilesController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,6 +28,7 @@ public class FilesController {
         this.fileRepository = fileRepository;
     }
 
+    @PreAuthorize("hasAuthority('TEACHER')")
     @PostMapping("/{id}")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file,
                                          @PathVariable String id) {
